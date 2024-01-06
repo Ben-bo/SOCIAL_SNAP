5fe773823a074a0e1e798fcd28ea54f7b56bbadc
Beni Iskandar <bennyizkandar@gmail.com>
2024-01-06 15:03:34 +0800

    update

---
+++ src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  // readonly VITE_APP_TITLE: string
+  readonly VITE_APPWRITE_PROJECTID: string;
+  readonly VITE_APPWRITE_URL: string;
+  readonly VITE_APPWRITE_URL: string;
+  readonly VITE_APPWRITE_PROJECTID: string;
+  readonly VITE_APPWRITE_STORAGE_ID: string;
+  readonly VITE_APPWRITE_DATABASE_ID: string;
+  readonly VITE_APPWRITE_SAVES_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_USERS_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_POSTS_COLLECTION_ID: string;
+  // more env variables...
+}
