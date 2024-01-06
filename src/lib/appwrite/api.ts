42c5029a9b62737e70b5e7c4a888001d91520412
Beni Iskandar <bennyizkandar@gmail.com>
2024-01-06 15:02:17 +0800

    lib: update appwrite and validation

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -10,8 +10,42 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
-    return newAccount;
+    if (!newAccount) throw new Error();
+    const avatarsUrl = avatars.getInitials(user.name);
+
+    const createUserDb = await saveUser({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      imageUrl: avatarsUrl,
+      name: user.name,
+      username: user.username,
+    });
+    return createUserDb;
   } catch (error) {
     console.log(error);
+    throw new Error("Failed Create New User");
+  }
+}
+
+export async function saveUser(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username: string;
+}) {
+  try {
+    const createUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return createUser;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
   }
 }
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const SignUpValidation = z.object({
   name: z.string().min(3),
-  username: z.string().min(2).max(50),
+  username: z.string().min(2, { message: "username to short." }).max(50),
   email: z.string().email(),
-  password: z.string().min(5),
+  password: z.string().min(8, { message: "password to short." }),
 });
