3748dfa4f9c1cdea240a2ce1a2e5b5edcbb484f3
Beni Iskandar <bennyizkandar@gmail.com>
2024-01-08 16:07:14 +0800

    lib:add auth context

---
+++ src/lib/react-query/QueryProvider.tsx
@@ -0,0 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      {" "}
+      {children}
+    </QueryClientProvider>
+  );
+};
